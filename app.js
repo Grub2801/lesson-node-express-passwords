950e74264cc1b957d1cfb52e87cc6278ccdbeff5
Philippa Johnstone <pecj@hotmail.co.uk>
2015-11-17 20:03:15 +0800

    in lab

--- app.js
+++ app.js
@@ -25,4 +25,63 @@ if (app.get('env') === 'development') {
   });
 }
 
+app.post("/signup", function(req, res) {
+
+  var userParams = request.body.user
+
+  if (!userParams.email) {
+    return res.status(401).send({message: "Missing email"});
+  }
+
+  if (!userParams.password) {
+    return res.status(401).send({message: "Missing password"});
+  }
+
+  if (userParams.password !== userParams.paswordConfrimation) {
+    return res.status(401).send({message: "Password does not match"});
+  }
+
+  User.findOne({ email: userParams.email }, function(err, user) {
+    if (user) {
+      return res.status(401).send({message: "email already in use"});
+    }
+  });
+
+  var userObject = new User(req.body.user);
+
+  userObject.save(function(err, user) {
+    if (err) {
+      return res.status(401).send({message: err.errmsg});
+    }
+    else {
+      return res.status(200).send({message: "user created"});
+    }
+  });
+});
+
+
+app.post("/signin", function(req, res) {
+  var userParams = req.body.user;
+
+  if (!userParams.email) {return res.status(401).send({message: "Missing email"}); }
+  if (!userParams.password) {return res.status(401).send({message: "Missing password"}); }
+
+  User.findOne({ email: userParams.email }, function(err, user) {
+    if (!user) {
+      return res.status(404).send({message: "Email is wrong"}); }
+
+    user.authenticate(userParams.password, function(err, isMatch) {
+      if (err) throw err;
+
+      if (isMatch) {
+        return res.status(200).send({message: "Valid Credentials !"});
+      }
+      else {
+        return res.status(401).send({message: "The credentials provided do not correspond to a registered user"});
+      }
+    });
+  });
+});
+
+
 app.listen(3000);
